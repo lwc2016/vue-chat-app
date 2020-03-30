83b7a840bcdf4f762a781ef5fbe64eff14c44f68
liuwenchao <liuwenchao1990@163.com>
2020-03-30 14:42:56 +0800

    提交代码

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const koa = require("koa");
 const http = require("http");
 const path = require("path");
+const compress = require("koa-compress");
 const staticSystem = require("koa-static");
 const bodyParser = require("koa-bodyparser");
 const app = new koa();
@@ -8,8 +9,13 @@ const route = require("./routes");
 const errorHandler = require("./middlewares/errorHandler.middleware");
 const sessionMiddleware = require("./middlewares/session.middleware")
 
+// 开启gzip压缩
+const options = { threshold: 2048 };
+app.use(compress(options));
 // 静态资源
-app.use(staticSystem(path.join(__dirname, "../dist")));
+app.use(staticSystem(path.join(__dirname, "../dist"), {
+    maxage: 7 * 24 * 60 * 60 * 1000
+}));
 // session
 app.use(sessionMiddleware(app));
 // 注册中间件
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const path = require("path");
 const router = new Router();
 router.get("*", async (ctx, next) => {
-    console.log(fs.readFileSync(path.join(__dirname, "../../dist/index.html")));
     ctx.type = "html";
     ctx.body = fs.readFileSync(path.join(__dirname, "../../dist/index.html"));
 });
