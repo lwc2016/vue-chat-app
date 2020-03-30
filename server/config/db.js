32777977f1cc1eaf33d6d942c92e243a8e766787
liuwenchao <liuwenchao1990@163.com>
2020-03-30 16:48:28 +0800

    提交代码

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,10 @@ const co = require("co-mysql");
 
 const connection = mysql.createPool({
     connectionLimit: 10,
-    host: "127.0.0.1",
+    host: "localhost",
     port: 3306,
     user: "root",
-    password: "12345678",
+    password: 12345678,
     database: "chat_app"
 });
 
